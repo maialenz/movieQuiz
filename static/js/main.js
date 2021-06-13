1d27e5e9fedd2382111224643c9d9300074a2268
maialenz <maialenzumeta@gmail.com>
2021-06-13 21:53:10 +0000

    created a dictionary with questions and answers

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    document.getElementById('#section2').style.display = "none"
+    document.getElementById("#section3").style.display = "none"
+
+    // hide all sections except the first one
+    $("#section2, #section3").hide()
+    $("#theme").show()
+
+        
+    })
---
+++ static/js/quiz.js
@@ -0,0 +1,204 @@
+const hpQuestions = [
+    {
+        question: "What is the name of Harry Potter's owl?",
+        answers: {
+            a: 'Scabbers',
+            b: 'Snowy',
+            c: 'Hedwig',
+            d: ''
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "What creature is Aragog?",
+        answers: {
+            a: 'Gigant House Spider',
+            b: 'Black Widow',
+            c: 'Tarantula',
+            d: 'Acromantula'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Who killed Dobby by throwing a knife at him?",
+        answers: {
+            a: 'Bellatrix Lestrange',
+            b: 'Alastor Moody',
+            c: 'Lucius Malfoy',
+            d: 'Voldemort'
+        },
+        correctAnswer: 'a'
+    }
+];
+
+const marvelQuestions = [
+    {
+        question: "What is the real name of the Scarlet Witch?",
+        answers: {
+            a: '',
+            b: 'Wanda Maximoff',
+            c: '',
+            d: ''
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which European city do Hawkeye and Black Widow often reminisce about?",
+        answers: {
+            a: '',
+            b: ' ',
+            c: '',
+            d: 'Budapest'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Complete the quote: 'That\'s my secret, Cap. I\'m always...'",
+        answers: {
+            a: 'green',
+            b: 'mad',
+            c: 'angry',
+            d: 'ready'
+        },
+        correctAnswer: 'c'
+    }
+];
+const starwarsQuestions = [
+    {
+        question: "Who are the only two characters who appear in every Star Wars movie?",
+        answers: {
+            a: 'Darth Vader and Luke',
+            b: 'Han Solo and Chewbacca',
+            c: 'Obi-Wan and Leia',
+            d: 'C-3PO and R2-D2'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Who played Mace Windu?",
+        answers: {
+            a: 'Samuel L. Jackson',
+            b: 'Liam Neeson',
+            c: 'Mark Hamill',
+            d: 'Hayden Cristensen'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Who built C-3PO?",
+        answers: {
+            a: 'Kylo Ren',
+            b: 'Anakin Skywalker',
+            c: 'Obi-Wan Kenobi',
+            d: 'Lando Calrissian'
+        },
+        correctAnswer: 'b'
+    }
+];
+
+
+
+/*const QUIZ = [
+    'harryPotter' [ {
+        id: cuid(),
+        question: 'How many times did spaceX try to reach orbit and failed before succeeding their first time?',
+        answer1: '0; they succeeding their first time.',
+        answer2: 'They succeeded their seconed time.',
+        answer3: '3; they succeeded their fourth time.',
+        answer4: '5; they succeeded their sixth time.',
+        correct: '3; they succeeded their fourth time.'
+    },
+
+    {
+        id: cuid(),
+        question: 'Which of these projects are part of spaceX?',
+        answer1: 'project titan',
+        answer2: 'project starlink',
+        answer3: 'project Planetary Defense',
+        answer4: 'project LightSail',
+        correct: 'project starlink'
+    },
+
+    {
+        id: cuid(),
+        question: 'Which of these is a spaceX rocket?',
+        answer1: 'Falcon Heavy',
+        answer2: 'Taurus',
+        answer3: 'Athena',
+        answer4: 'Pegasus',
+        correct: 'Falcon Heavy'
+    },
+
+    {
+        id: cuid(),
+        question: 'When was SpaceX founded?',
+        answer1: 'In the 1950\'s',
+        answer2: 'In 1999',
+        answer3: 'In 2002',
+        answer4: 'In 2010',
+        correct: 'In 2002'
+    },
+
+
+    {
+        id: cuid(),
+        question: 'When was the first time SpaceX launched a satellite to geostationary orbit?',
+        answer1: 'In December 2013.',
+        answer2: 'In March 2011.',
+        answer3: 'In November 2010.',
+        answer4: 'In June 2009',
+        correct: 'In December 2013.'
+    }
+]; */
+
+/*
+{
+	'batman' : [
+	 {
+	 	'image_url': 'test.coom',
+	 	'question': 'whats his original name ?',
+	 	'answers': ['abc', 'Bruce', 'lmn', 'opq'],
+	 	'right_answer': 'Bruce'	
+
+	 },
+	 {
+	 	'image_url': 'test1.coom',
+	 	'question': 'whats his car name ?',
+	 	'answers': ['abc', 'Bruce', 'lmn', 'opq'],
+	 	'right_answer': 'Bruce'	
+
+	 },
+	 {
+	 	'image_url': 'test2.coom',
+	 	'question': 'whats his bike name ?',
+	 	'answers': ['abc', 'Bruce', 'lmn', 'opq'],
+	 	'right_answer': 'Bruce'	
+
+	 }
+
+	],
+	'superman' : [
+	 {
+	 	'image_url': 'test.coom',
+	 	'question': 'whats his original name ?',
+	 	'answers': ['abc', 'Bruce', 'lmn', 'opq'],
+	 	'right_answer': 'Bruce'	
+
+	 },
+	 {
+	 	'image_url': 'test1.coom',
+	 	'question': 'whats his car name ?',
+	 	'answers': ['abc', 'Bruce', 'lmn', 'opq'],
+	 	'right_answer': 'Bruce'	
+
+	 },
+	 {
+	 	'image_url': 'test2.coom',
+	 	'question': 'whats his bike name ?',
+	 	'answers': ['abc', 'Bruce', 'lmn', 'opq'],
+	 	'right_answer': 'Bruce'	
+
+	 }
+
+	]
+} */
