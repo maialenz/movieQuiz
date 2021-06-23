a261b4756ea9b8ed2cab69a455c89a2cec5ad73c
maialenz <maialenzumeta@gmail.com>
2021-06-23 12:26:51 +0000

    changed const variables for let variables and commented function

--- static/js/highscores.js
+++ static/js/highscores.js
@@ -1,6 +1,11 @@
 let highScoresList = document.getElementById("highScoresList");
 let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+/**
+ * targets the highScoresList on the html and changes it's inner HTML by a template literals to be able
+ * to display a list with the name and score saved on the end.js.
+ * Everytime a user inputs their name it creates a new list and displays it at the bottom of the previous one
+ */
 
 highScoresList.innerHTML = highScores
     .map(score => {
